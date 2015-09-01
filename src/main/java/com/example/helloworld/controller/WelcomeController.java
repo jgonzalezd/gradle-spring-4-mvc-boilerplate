37cec307ed504019fcc3142f805814c8a4a4acf2
Julian Gonzalez <jgonzalez@localhost.localdomain>
2015-09-01 16:50:46 -0500

    Initial commit

---
+++ src/main/java/com/example/helloworld/controller/WelcomeController.java
@@ -0,0 +1,38 @@
+package com.example.helloworld.controller;
+
+import com.example.helloworld.service.HelloWorldService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Map;
+
+/**
+ * Created by jgonzalez on 9/1/15.
+ */
+@Controller
+public class WelcomeController {
+
+    private final Logger logger = LoggerFactory.getLogger(WelcomeController.class);
+    private final HelloWorldService helloWorldService;
+
+    @Autowired
+    public WelcomeController(HelloWorldService helloWorldService) {
+        this.helloWorldService = helloWorldService;
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index(Map<String, Object> model) {
+
+        logger.debug("index() is executed!");
+
+        model.put("title", helloWorldService.getTitle(""));
+        model.put("msg", helloWorldService.getDesc());
+
+        return "index";
+    }
+
+}
---
+++ src/main/java/com/example/helloworld/service/HelloWorldService.java
@@ -0,0 +1,39 @@
+package com.example.helloworld.service;
+
+
+
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+/**
+ * Created by jgonzalez on 9/1/15.
+ */
+@Service
+public class HelloWorldService {
+
+    private static final Logger logger = LoggerFactory.getLogger(HelloWorldService.class);
+
+    public String getDesc() {
+
+        logger.debug("getDesc() is executed!");
+
+        return "Gradle + Spring MVC Hello World Example";
+
+    }
+
+    public String getTitle(String name) {
+
+        logger.debug("getTitle() is executed! $name : {}", name);
+
+        if(StringUtils.isEmpty(name)){
+            return "Hello World";
+        }else{
+            return "Hello " + name;
+        }
+
+    }
+}
